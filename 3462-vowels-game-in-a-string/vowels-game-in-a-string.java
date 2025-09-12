225ed48719bd016ebad357cfadf16d276fe5dbc8
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2025-09-12 13:41:53 +0530

    Time: 4 ms (79.79%) | Memory: 45.7 MB (45.41%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -6,16 +6,13 @@ for (int i = 0; i < s.length(); i++) {
     char ch = s.charAt(i);
     if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
         ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-        count++;
+       return true;
     }
 }
 
-        if (count==0)return false;
-      if ((count%3==0)&&(count%3)/2==1){
-        return false;
-      }
+     
     
-      return true;
+      return false;
     }
    
 }

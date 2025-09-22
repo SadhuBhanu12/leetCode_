889cbb30537f6eb3527e7a797a2245c0c48eb5d1
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2025-09-22 10:55:33 +0530

    Time: 3 ms (26.97%) | Memory: 42.4 MB (56.53%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -4,27 +4,19 @@ class Solution {
     public int maxFrequencyElements(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         ArrayList<Integer> list1 = new ArrayList<>();
-
-        // collect unique elements
         for (int i = 0; i < nums.length; i++) {
             set.add(nums[i]);
         }
-
         for (int i : set) {
             list1.add(i);
         }
 
         int arr[] = new int[list1.size()];
-
+        int max=0;
         for (int i = 0; i < nums.length; i++) {
             arr[list1.indexOf(nums[i])] += 1;
+            max=Math.max(arr[list1.indexOf(nums[i])],max);
         }
-
-        int max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            max = Math.max(arr[i], max);
-        }
-
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == max) sum += arr[i];

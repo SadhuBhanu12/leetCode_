0be47657c504219d21901a950780dc9be2a9ddca
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2025-09-23 12:02:24 +0530

    Time: 1 ms (76.03%) | Memory: 41.4 MB (83.19%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,27 +1,16 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
         String arr1[]=version1.split("\\.");
-          String arr2[]=version2.split("\\.");
-          int c=0;
-          for (int i=0;i<Math.max(arr1.length,arr2.length);i++){
-           int a1 = (i < arr1.length) ? Integer.parseInt(arr1[i]) : 0;
-            int a2 = (i < arr2.length) ? Integer.parseInt(arr2[i]) : 0;
-            System.out.println(a1+"  "+a2);
-            if (a1>a2)return  1;
-            else if (a2>a1)return -1;
-            else if (a1==a2)c++; 
-          }
-          
-          if(arr1.length>arr2.length){
-            for (int i=c;i<arr1.length;i++){
-                if (Integer.parseInt(arr1[i])!=0)return 1;
-            }
-          }
-          if(arr2.length>arr1.length){
-            for (int i=c;i<arr2.length;i++){
-                if (Integer.parseInt(arr2[i])!=0)return -1;
-            }
-          }
-          return 0;
+        String arr2[]=version2.split("\\.");
+        int arr11[]=new int[Math.max(arr1.length,arr2.length)];
+        int arr12[]=new int[Math.max(arr1.length,arr2.length)];
+        for (int i=0;i<arr1.length;i++)arr11[i]=Integer.parseInt(arr1[i]);
+        for (int i=0;i<arr2.length;i++)arr12[i]=Integer.parseInt(arr2[i]);
+        for (int i=0;i<arr11.length;i++){
+            if (arr11[i]>arr12[i])return 1;
+            if (arr12[i]>arr11[i])return -1;
+        }
+        return 0;
+
     }
 }

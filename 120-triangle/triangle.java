cd1c7501d7423ebaa669408daf46176140c104a7
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2025-09-25 13:41:13 +0530

    Time: 11 ms (6.00%) | Memory: 44.9 MB (98.69%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,13 +1,26 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        for (int i=triangle.size()-2;i>=0;i--){
-            for (int j=0;j<triangle.get(i).size();j++){
-                int pres=triangle.get(i).get(j);
-                int belo=triangle.get(i+1).get(j);
-                int belorigh=triangle.get(i+1).get(j+1);
-                triangle.get(i).set(j,pres+Math.min(belo,belorigh));
+        if (triangle.size()==1)return triangle.get(0).get(0);
+        if (triangle.size()==2)return Math.min(triangle.get(0).get(0)+triangle.get(1).get(0),triangle.get(0).get(0)+triangle.get(1).get(1));
+        List<List<Integer>> list=new ArrayList<>();
+        list.add(new ArrayList<>(List.of(triangle.get(0).get(0))));
+        list.add(new ArrayList<>(List.of(triangle.get(0).get(0)+triangle.get(1).get(0),triangle.get(0).get(0)+triangle.get(1).get(1))));
+        for (int i=2;i<triangle.size();i++){
+            List<Integer> pre=list.get(i-1);
+            ArrayList<Integer> after=new ArrayList<>();
+            after.add(pre.get(0)+triangle.get(i).get(0));
+            for (int j=1;j<triangle.get(i).size()-1;j++){
+                 after.add(triangle.get(i).get(j) + Math.min(pre.get(j-1), pre.get(j)));
             }
+            after.add(pre.get(pre.size()-1)+triangle.get(i).get(triangle.get(i).size()-1));
+            list.add(after);
         }
-        return triangle.get(0).get(0);
+        System.out.println(list+"");
+        int min=Integer.MAX_VALUE;
+        int size1=list.size()-1;
+        for (int i=0;i<list.get(list.size()-1).size();i++){
+            min=Math.min(min,list.get(size1).get(i));
+        }
+return min;
     }
 }

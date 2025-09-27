6606d338f9277017f09031b9dc12fd2437aa04ac
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2025-09-27 11:37:43 +0530

    Time: 8 ms (62.33%) | Memory: 41.6 MB (54.40%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.java
+++ 830-largest-triangle-area/largest-triangle-area.java
@@ -8,12 +8,11 @@ class Solution {
                     int x1 = points[i][0], y1 = points[i][1];
                     int x2 = points[j][0], y2 = points[j][1];
                     int x3 = points[k][0], y3 = points[k][1];
-
                     double dis = Math.abs(
                         x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)
                     ) * 0.5;
 
-                    if (dis > max1) max1 = dis;
+                   max1=Math.max(dis,max1);
                 }
             }
         }

1f318d9ddbf6a3bc36f0d5b67b081bf919be30bf
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2025-10-14 11:58:44 +0530

    Time: 5 ms (96.36%) | Memory: 45 MB (90.09%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,33 +1,29 @@
 class Solution {
     public String frequencySort(String s) {
-        int arr[] = new int[128]; 
-        for (int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i)]++; 
+        int arr[]=new int[128];
+        for (int i=0;i<s.length();i++){
+            arr[s.charAt(i)]++;
         }
+        
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<s.length();i++){
+            int max=0;
+            int r=-1;
+            for (int j=0;j<arr.length;j++){
+                if (arr[j]>=max && arr[j]!=0){
+                max=arr[j];
 
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            int max = 0;
-            int r = -1;
-
-      
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] > max) {
-                    max = arr[j];
-                    r = j;
+                r=j;
+                
                 }
             }
-
             if (r == -1 || max == 0) break;
-
-            for (int k = 0; k < max; k++) {
-                sb.append((char) r);
+            for (int k=0;k<max;k++){
+                sb.append((char)r);
             }
-
-            arr[r] = 0; 
+            arr[r]=0;
+           
         }
-
         return sb.toString();
     }
 }

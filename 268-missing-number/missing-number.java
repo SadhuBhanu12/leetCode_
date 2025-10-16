79806ea12730fefdc42c84f15f9890c9e4353cd5
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2025-10-16 13:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (22.47%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,14 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        TreeSet<Integer> set=new TreeSet<>();
-        for (int i=0;i<nums.length;i++)set.add(nums[i]);
-        int c=0;
-       
-        for (int i:set){
-            if ((c)!=i){
-                return c;
-            }c++;
-        }
-        return set.size();
+       int res=nums.length;
+       for (int i=0;i<nums.length;i++){
+        int a=i^(nums[i]);
+        res=res^(a);
+       }
+       return res;
     }
 }

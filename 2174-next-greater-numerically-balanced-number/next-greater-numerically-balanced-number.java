17d20a42f8b24deb3af602a89f094e3671642ab2
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2025-10-24 14:21:08 +0530

    Time: 638 ms (5.39%) | Memory: 44.3 MB (75.38%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -1,21 +1,19 @@
 class Solution {
     public int nextBeautifulNumber(int n) {
-        for (int i = n + 1; i <=999999999 ; i++) {
-            if (check(i)) {
+        for (int i=n+1;i<=1224444 ;i++){
+            if (check(Integer.toString(i))){
                 return i;
             }
         }
         return -1;
     }
-
-    public boolean check(int a) {
-        int arr[] = new int[10];
-        while (a > 0) {
-            arr[a % 10]++;
-            a = a / 10;
+    public boolean check(String a){
+        HashMap<Character,Integer> map=new HashMap<>();
+        for ( char ch:a.toCharArray()){
+                map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != i && arr[i]>0) return false;
+        for(char ch:map.keySet()){
+            if (Integer.parseInt(ch+"")!=map.get(ch))return false;
         }
         return true;
     }

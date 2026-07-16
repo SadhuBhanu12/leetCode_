a422e9a9440a038b35dab1ff0ba129a5a7e5cfcd
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2026-07-16 23:48:39 +0530

    Time: 2 ms (92.19%) | Memory: 47.9 MB (20.29%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,9 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int count=0;
-        int iter=0;
-        for (int i=0;i<nums.length;i++){
-            if (nums[i]!=0){
-                nums[iter++]=nums[i];
-            }
-            else{
-                count++;
-            }
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+                if(nums[i]!=0)nums[c++]=nums[i];
         }
-        for (int i=iter;i<nums.length;i++){
-            nums[i]=0;
-        }
-
+        Arrays.fill(nums,c,nums.length,0);
     }
 }

2e9a957d68759784655ec8475f60083d0e69dc1e
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2026-07-17 23:24:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (46.84%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,24 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0, r = nums.length - 1;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if (nums[mid] == target) return mid;
-
-            if (nums[l] <= nums[mid]) {
-                if (nums[l] <= target && target < nums[mid]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            } else {
-                if (nums[mid] < target && target <= nums[r]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+        int index=-1;
+        for(int i=0;i<nums.length-1;i++)if(nums[i]>nums[i+1]){
+            index=i+1;
+            break;
+        }
+        if (index == -1)
+    return binary(nums, 0, nums.length - 1, target);
+        if(nums[0]<=target && target<=nums[index-1])return binary(nums,0,index-1,target);
+        return binary(nums,index,nums.length-1,target);
+    }
+    public int binary(int nums[],int l1,int r1,int target){
+        int l=l1;
+        int r=r1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else if(nums[mid]>target){
+                r=mid-1;
             }
         }
         return -1;

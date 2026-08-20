86ff79404bb122ed7be47fd3ac4b89dd12a42f8c
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2026-08-20 18:57:09 +0530

    Time: 46 ms (6.03%) | Memory: 48.8 MB (92.64%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,36 +1,31 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
-
-        ArrayList<int[]> list = new ArrayList<>();
-
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        list.add(new int[]{intervals[0][0], intervals[0][1]});
-
-        for (int i = 1; i < intervals.length; i++) {
-
-            int n12 = list.get(list.size() - 1)[1];
-            int n21 = intervals[i][0];
-            int n22 = intervals[i][1];
-
-            if (n21 <= n12) {
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        ArrayList<int[]> list=new ArrayList<>();
+        list.add(new int[]{intervals[0][0],intervals[0][1]});
+        for(int i=1;i<intervals.length;i++){
+            System.out.println("bhanu");
+            int n21=intervals[i][0];
+            int n22=intervals[i][1];
+         
+            int n12=list.get(list.size()-1)[1];
+            if(n21<=n12){
                 
                 list.get(list.size() - 1)[1] = Math.max(n12, n22);
-            } else {
-             
-                list.add(new int[]{n21, n22});
+            
+            }
+        
+            else{
+                list.add(new int[]{n21,n22});
             }
         }
+    int[][] arr = new int[list.size()][2];
 
-        int[][] arr = new int[list.size()][2];
-
-        for (int i = 0; i < list.size(); i++) {
-            arr[i][0] = list.get(i)[0];
-            arr[i][1] = list.get(i)[1];
-        }
-
-        return arr;
+for (int i = 0; i < list.size(); i++) {
+    for (int j = 0; j < 2; j++) {
+        arr[i][j] = list.get(i)[j];
+    }
+}
+return arr;
     }
 }

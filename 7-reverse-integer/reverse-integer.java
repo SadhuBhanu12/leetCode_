1f7f1fbbeb92812d4000ab637e557c2108b5687b
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2026-08-20 13:30:50 +0530

    Time: 2 ms (7.02%) | Memory: 42.8 MB (27.03%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,30 +1,24 @@
 class Solution {
     public int reverse(int x) {
-        if (x == 0) return 0;
-
-        int val = x;
-        long num = Math.abs((long) x);
-
-        Queue<Integer> queue = new LinkedList<>();
-
-        while (num > 0) {
-            int temp = (int)(num % 10);
-            num = num / 10;
+        if(x==0)return 0;
+        int val=x;
+        long num=Math.abs((long)x);
+        Queue<Integer> queue=new LinkedList<>();
+        while(num>0){
+            int temp=(int)(num%10);
+            num=num/10;
             queue.offer(temp);
         }
 
-        long val1 = queue.poll();
 
-        while (!queue.isEmpty()) {
-            int poll = queue.poll();
-            val1 = (val1 * 10) + poll;
+        long val1=queue.poll();
+        int curr=1;
+        while(!queue.isEmpty()){
+            long poll=queue.poll();
+            val1=(val1*10)+poll;
         }
-
-        val1 = (val > 0) ? val1 : -val1;
-
-        if (val1 < -(1L << 31) || val1 > (1L << 31) - 1)
-            return 0;
-
-        return (int) val1;
+        val1=(val>0)?val1:-1*val1;
+        if(val1<-1*Math.pow(2,31) || val1>Math.pow(2,31)-1)return 0;
+        return (int)val1;
     }
 }

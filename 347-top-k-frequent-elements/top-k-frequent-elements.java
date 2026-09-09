3c535e919afa48ac0c14153014878ce8816304fb
SADHU BHANU PRASAD <156383358+SadhuBhanu12@users.noreply.github.com>
2026-09-09 22:36:38 +0530

    Time: 15 ms (54.88%) | Memory: 47.8 MB (33.37%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-       
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-    PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)-> Integer.compare(a[1],b[1]));
-    for(int i:map.keySet()){
-        pq.offer(new int[]{i,map.get(i)});
-    }
-    int count=0;
-    int arr[]=new int[map.size()];
-    while(!pq.isEmpty()){
-        int arr1[]=pq.poll();
-        arr[count++]=arr1[0];
-    }
-    int res[]=new int[k];
-    int c1=0;
-    for(int i=arr.length-1;i>=0;i--){
-            if(c1==k)break;
+        int arr[][]=new int[map.size()][2];
+        int c=0;
+        for(int i:map.keySet()){
+            arr[c][0]=i;
+            arr[c][1]=map.get(i);
+            c++;
+        }
+        Arrays.sort(arr,(a,b)-> Integer.compare(a[1],b[1]));
+        int res[]=new int[k];
+        int count=0;
+        for(int i=arr.length-1;i>=0;i--){
+            
+            if(count>=k)break;
             else{
-                res[c1++]=arr[i];
+                res[count]=arr[i][0];
+                count++;
             }
-    }
+        }
         return res;
+
     }
 }
